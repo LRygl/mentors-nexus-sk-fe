5a232f3db383124b6d5af2e496c00f7df84690a8
lrygl <lubomir.rygl@gmail.com>
2025-01-23 21:44:32 +0100

    create navbar

--- src/lib/components/main-nav.svelte
+++ src/lib/components/main-nav.svelte
@@ -1,11 +1,27 @@
-<nav>
-	<a class="nav-link active" href="/">HOME</a>
+<nav class="flex flex-row justify-around items-center bg-gray-200 dark:bg-gray-800 h-14 text-white">
+	<div>
+		<a href="/" class="text-white">LOGO</a>
+	</div>
+	<div>
 
-	Public
-	Admin
+		<ul class="flex">
+			<li class="mr-6 hover:rounded-3xl p-3 hover:bg-amber-300"><a class="nav-link active" href="/">HOME</a></li>
+			<li class="mr-6"><a class="nav-link active" href="/admin/users">USERS</a></li>
+			<li class="mr-6"><a class="nav-link active" href="/admin/ideas">IDEAS</a></li>
+			<li class="mr-6">Active</li>
 
-	<a class="nav-link active" href="/admin/users">USERS</a>
-	<a class="nav-link active" href="/admin/ideas">IDEAS</a>
+		</ul>
+
+
+
+
+
+	</div>
+	<div>
+		<a class="nav-link active" href="/admin/users">Register</a>
+		<a class="nav-link active" href="/admin/users">Login</a>
+
+	</div>
 
 </nav>
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,15 @@
 <script>
 	import "../app.css";
 	import MainNav from "$lib/components/main-nav.svelte";
+	import Banner from "$lib/components/banner.svelte";
 </script>
 
+<Banner />
 <MainNav/>
 <slot/>
+
+
+
+<style>
+
+</style>
